9ecda920218e872c796c0966883c783e656ca9ed
shobdestiny1 <97878301+shobdestiny1@users.noreply.github.com>
2022-02-18 21:42:57 +0530

    Delete Backend directory

--- Backend/Signatures.js
+++
@@ -1,8 +0,0 @@
-const mongoose =require('mongoose');
-
-const serviceSchema = new mongoose.Schema({
-    address: String,
-    signature: String
-})
-
-module.exports = mongoose.model('Signatures', serviceSchema)
