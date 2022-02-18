d89752073e951913405d25869c045c24399ed271
shobdestiny1 <97878301+shobdestiny1@users.noreply.github.com>
2022-02-18 21:43:16 +0530

    Delete Signer directory

--- Signer/List.js
+++
@@ -1,5 +0,0 @@
-module.exports =
-[
-    "0x864AB3B9cBDA971ce961F315EcF69414F6BF9dab"
-
-]
--- Signer/Signatures.js
+++
@@ -1,7 +0,0 @@
-const mongoose =require('mongoose');
-
-const serviceSchema = new mongoose.Schema({
-    signature: String,
-})
-
-module.exports = mongoose.model('Signatures', serviceSchema)
