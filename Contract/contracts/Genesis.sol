2e8700740e30398faef3d701da53e73179886d43
AceVikings <akash@0xytocin.com>
2022-02-25 11:34:21 +0530

    Update Genesis.sol

--- Contract/contracts/Genesis.sol
+++ Contract/contracts/Genesis.sol
@@ -25,6 +25,7 @@ contract Genesis is ERC721Enumerable, Ownable, ReentrancyGuard {
     // event Mint(address indexed to, uint256 amount);
     
     event Claim(uint256[] tokenIds);
+    uint[] public tokenToOpenseaMap;
 
     event SetBaseTokenURI(string baseTokenURI);
 
@@ -36,21 +37,22 @@ contract Genesis is ERC721Enumerable, Ownable, ReentrancyGuard {
     ) ERC721(_name, _symbol) {
         setBaseURI(_baseURI);
         setOldGenesis(_oldGenesis);
+        tokenToOpenseaMap = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255, 256, 257, 258, 259, 260, 261, 262, 263, 264, 265, 266, 267, 268, 269, 270, 271, 272, 273, 274, 275, 276, 277, 278, 279, 280, 281, 282, 283, 284, 285, 286, 287, 288, 289, 290, 291, 292, 293, 294, 295, 296, 297, 298, 299, 300, 301, 302, 303, 304, 305, 306, 307, 308, 309, 310, 311, 312, 313, 314, 315, 316, 317, 318, 319, 320, 321, 322, 323, 324, 325, 326, 327, 328, 329, 330, 331, 332, 333];
     }
 
-    /**
-     * @dev Mint NFTs
-     */
-    function mint(uint256[] memory _tokens) external nonReentrant onlyOwner{
-        require(_tokens.length > 0, "genesis : mint amount invalid");
-        require(totalSupply() + _tokens.length <= CAP, "genesis : max limit");
+    // /**
+    //  * @dev Mint NFTs
+    //  */
+    // function setTokens(uint256[] memory _tokens) external nonReentrant onlyOwner{
+    //     require(_tokens.length > 0, "genesis : mint amount invalid");
+    //     require(tokenToOpenseaMap.length + _tokens.length <= CAP, "genesis : max limit");
 
-        for (uint256 i = 0; i < _tokens.length; i++) {
-            _safeMint(nftOwner, _tokens[i]);
-        }
+    //     for (uint256 i = 0; i < _tokens.length; i++) {
+    //         tokenToOpenseaMap.push(_tokens[i]);
+    //     }
 
-        // emit Mint(nftOwner, _amount);
-    }
+    //     // emit Mint(nftOwner, _amount);
+    // }
 
     /**
      * @dev Claim NFT
@@ -63,13 +65,14 @@ contract Genesis is ERC721Enumerable, Ownable, ReentrancyGuard {
 
             require(tokenId != 0, "genesis : invalid tokenId");
 
-            uint256 count = IERC1155(oldGenesis).balanceOf(msg.sender, tokenId);
+            uint256 count = IERC1155(oldGenesis).balanceOf(msg.sender, tokenToOpenseaMap[tokenId-1]);
 
             require(count > 0, "genesis : sender is not owner");
 
-            IERC1155(oldGenesis).safeTransferFrom(msg.sender, nftOwner, tokenId, 1, "");
-            
-            super._safeTransfer(nftOwner, msg.sender, tokenId, "");
+            IERC1155(oldGenesis).safeTransferFrom(msg.sender, nftOwner, tokenToOpenseaMap[tokenId-1], 1, "");
+
+            _safeMint(msg.sender,tokenId);
+            // super._safeTransfer(nftOwner, msg.sender, tokenId, "");
         }
 
         emit Claim(_tokenIds);
