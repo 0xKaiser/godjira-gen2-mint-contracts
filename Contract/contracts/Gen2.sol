b11cac220ff8875811b564f58b53c20b64cc3969
baba0xytocin <baba@0xytocin.com>
2022-03-03 10:53:17 +0300

    Update gen2

--- Contract/contracts/Gen2.sol
+++ Contract/contracts/Gen2.sol
@@ -4,128 +4,99 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "erc721a/contracts/ERC721A.sol";
+import "hardhat/console.sol";
 
 contract Gen2 is ERC721A, Ownable, Pausable, ReentrancyGuard {
-    /// @dev token id tracker
-    uint256 public tokenIdTracker;
-
+    using SafeMath for uint256;
     /// @dev Maximum elements
     uint256 CAP = 3333;
+
+    /// @dev Token Price
+    uint256 price = 99;
+    uint256 tokenPrice = price.div(1000);
 
     /// @dev baseTokenURI
     string public baseTokenURI;
 
-    address public immutable legendaryAddress =
-        0xF6f94e2faAb5D415f40a4755860C788df050a37c;
-    address public immutable godjiraAddress =
-        0x3B0C7fb36cCf7bB203e5126B2192371Af91831BF;
+    /// @dev Genesis address
+    address public genesisAddress;
+
+    address public constant CORE_TEAM_ADDRESS = 0xC79b099E83f6ECc8242f93d35782562b42c459F3;
+    address public constant FOUNDER_SHAN_ADDRESS = 0xAd7Bbe006c8D919Ffcf6148b227Bb692F7D1fbc7;
+    address public constant FOUNDER_JAMIE_ADDRESS = 0x2dFa24018E419eA8453190155434D35328A8c6d8;
 
     /// @dev wallet address => whitelist status
     mapping(address => bool) public whitelist;
+
+    /// @dev wallet address => status
+    mapping(address => bool) public privateSaleBuyers;
+
     mapping(address => uint256) public nfts;
 
-    event Minted(address indexed to, uint256 tokenId);
     event SetBaseTokenURI(string baseTokenURI);
-    event Emited(address white, bool status);
-
-    /**
-     * @dev Allow mint when sale is open
-     */
-    modifier saleIsOpen() {
-        require(tokenIdTracker <= CAP, "gen2: sale cap exceed");
-        if (msg.sender != owner()) {
-            require(!paused(), "gen2: paused");
-        }
-        _;
-    }
+    event AddedWhitelist(address whitelistWallet, bool status);
+    event RemovedWhitelist(address whitelistWallet, bool status);
 
     constructor(
         string memory _name,
         string memory _symbol,
-        string memory baseToken_
+        string memory baseToken_,
+        address _genesis
     ) ERC721A(_name, _symbol) {
         setBaseURI(baseToken_);
+        setGenesis(_genesis);
+
+        // CoreTeam -> tokenIds #1 ~ #310
+        _safeMint(CORE_TEAM_ADDRESS, 310);
+         // Founder Shan -> tokenIds #311 ~ #325
+        _safeMint(FOUNDER_SHAN_ADDRESS, 15);
+        // Founder Jamie -> tokenIds #326 ~ #340
+        _safeMint(FOUNDER_JAMIE_ADDRESS, 15);
+
+        _safeMint(CORE_TEAM_ADDRESS, 2993);
     }
 
     /**
-     * @dev Mint NFTs
-     */
-    function mint(uint256 _amount) external nonReentrant saleIsOpen {
-        uint256 _tokenIdTracker = tokenIdTracker;
-
-        require(_amount > 0, "gen2 : mint amount invalid");
-        require(_tokenIdTracker + _amount <= CAP, "gen2 : max limit");
-
-        // TODO check for loop
-        if (_tokenIdTracker + _amount < 11) {
-            require(owner() == msg.sender, "gen2: caller is not the owner");
-
-            for (uint256 i = 0; i < _amount; i++) {
-                tokenIdTracker = _tokenIdTracker + 1;
-                _safeMint(legendaryAddress, _tokenIdTracker + 1);
-
-                emit Minted(legendaryAddress, _tokenIdTracker + 1);
-                _tokenIdTracker++;
-            }
-            
-            nfts[legendaryAddress] = _tokenIdTracker;
-        } else if (_tokenIdTracker + _amount < 111) {
-            require(owner() == msg.sender, "gen2: caller is not the owner");
-
-            for (uint256 i = 0; i < _amount; i++) {
-                tokenIdTracker = _tokenIdTracker + 1;
-                _safeMint(godjiraAddress, _tokenIdTracker + 1);
-
-                emit Minted(godjiraAddress, _tokenIdTracker + 1);
-                _tokenIdTracker++;
-            }
-
-            nfts[godjiraAddress] = _tokenIdTracker;
-        } else if (_tokenIdTracker < 1301) {
-            require(
-                msg.sender != legendaryAddress && msg.sender != godjiraAddress,
-                "gen2: legendary, godjira unavailable"
-            );
-            require(_amount == 1, "gen2: public invalid amount");
-            require(nfts[msg.sender] == 0, "gen2: public invalid address");
-
-            tokenIdTracker = _tokenIdTracker + 1;
-            _safeMint(msg.sender, _tokenIdTracker + 1);
-            nfts[msg.sender] = _tokenIdTracker + 1;
-
-            emit Minted(msg.sender, _amount);
-        } else if (_tokenIdTracker < 2901) {
-            require(
-                msg.sender != legendaryAddress && msg.sender != godjiraAddress,
-                "gen2: legendary, godjira unavailable"
-            );
-            require(_amount == 1, "gen2: whitelist invalid amount");
-            require(
-                whitelist[msg.sender],
-                "gen2: whitelist caller not whitelisted"
-            );
-            require(nfts[msg.sender] == 0, "gen2: whitelist invalid address");
-
-            tokenIdTracker = _tokenIdTracker + 1;
-            _safeMint(msg.sender, _tokenIdTracker + 1);
-            nfts[msg.sender] = _tokenIdTracker + 1;
-
-            emit Minted(msg.sender, _amount);
-        } else if (_tokenIdTracker >= 2901) {
-            require(
-                msg.sender != legendaryAddress && msg.sender != godjiraAddress,
-                "gen2: legendary, godjira unavailable"
-            );
-            require(_amount == 1, "gen2: free invalid amount");
-            require(nfts[msg.sender] == 0, "gen2: free invalid address");
-
-            tokenIdTracker = _tokenIdTracker + 1;
-            _safeMint(msg.sender, _tokenIdTracker + 1);
-            nfts[msg.sender] = _tokenIdTracker + 1;
-
-            emit Minted(msg.sender, _amount);
+     * @dev Purchase NFTs
+    */
+    function purchase() external nonReentrant payable {
+        bool genesisHolder = isGenesisHolder(msg.sender);
+        
+        /**
+        * Private sale buyers
+        * Total amount : 100 (1pw)
+        * TokenIds : #341 ~ #440
+        * Start time : 9 Mar 2am ~ --
+        */
+        if(!whitelist[msg.sender]) {
+            console.log("Private sale buyers");
         }
+
+        /**
+        * Genesis Holders 
+        * Total amount : 666 (2pw)
+        * TokenIds : #441 ~ #1106
+        * Start time 9 Mar 4am ~ --
+        */
+        if(genesisHolder) {
+            console.log("GenesisHolder");
+        }
+
+        /**
+        * Whitelist wallets
+        * Total amount : 1747 (1pw)
+        * TokenIds : #1107 ~ #2853
+        * Start time 10 Mar 4am ~ 11 Mar 4am
+        */
+        if(whitelist[msg.sender]) {
+            console.log("Whitelisted");
+        }
+
+        
+
     }
 
     /**
@@ -147,25 +118,44 @@ contract Gen2 is ERC721A, Ownable, Pausable, ReentrancyGuard {
     }
 
     /**
-     * @dev Add 'whitelist'
-     */
-    function addWhiteList(address[] memory _accounts) public onlyOwner {
-        for (uint256 i = 0; i < _accounts.length; i++) {
-            if (_accounts[i] != address(0)) whitelist[_accounts[i]] = true;
-
-            emit Emited(_accounts[i], true);
-        }
+     * @dev Check if wallet address owns any genesis tokens.
+     * @param account address
+    */
+    function isGenesisHolder(address account) public view returns (bool) {
+        return IERC721(genesisAddress).balanceOf(account) > 0;
     }
 
     /**
      * @dev Add 'whitelist'
-     */
+     * @param _accounts array of address
+    */
+    function addWhiteList(address[] memory _accounts) public onlyOwner {
+        for (uint256 i = 0; i < _accounts.length; i++) {
+            require(isGenesisHolder(_accounts[i]), "gen2 : should be not genesis holder");
+            if (_accounts[i] != address(0)) whitelist[_accounts[i]] = true;
+
+            emit AddedWhitelist(_accounts[i], true);
+        }
+    }
+
+    /**
+     * @dev Remove 'whitelist'
+     * @param _accounts array of address
+    */
     function removeWhiteList(address[] memory _accounts) public onlyOwner {
         for (uint256 i = 0; i < _accounts.length; i++) {
-            whitelist[_accounts[i]] = false;
+            if (_accounts[i] != address(0)) delete whitelist[_accounts[i]];
 
-            emit Emited(_accounts[i], false);
+            emit RemovedWhitelist(_accounts[i], false);
         }
+    }
+
+    /**
+     * @dev set address of Genesis
+     * @param _genesis address of genesis
+    */
+    function setGenesis(address _genesis) public onlyOwner {
+        genesisAddress = _genesis;
     }
 
     function pause() external whenNotPaused onlyOwner {
--- Contract/test/gen2.test.js
+++ Contract/test/gen2.test.js
@@ -10,58 +10,61 @@ describe.only('Gen2', () => {
     this.tokenUri = "https://gladiators-metadata-api.herokuapp.com/api/token"
     this.deployer = users[0]
     this.users = users.slice(1)
+
+    const genesis = await ethers.getContractFactory('Genesis')
+    const mockNFT = await ethers.getContractFactory('MockNFT')
+
+    this.mockNFT = await mockNFT.deploy()
+    this.genesis = await genesis.deploy("genesis", "genesis", this.tokenUri, this.mockNFT.address)
     
     const gen2 = await ethers.getContractFactory('Gen2')
-    this.gen2 = await gen2.deploy("gen2", "gen2", this.tokenUri)
+    this.gen2 = await gen2.deploy("gen2", "gen2", this.tokenUri, this.genesis.address)
 
-    // await this.gen2.connect(this.deployer).mint([1])
-  })
+    await this.genesis.connect(this.deployer).mint([1])
+    await this.mockNFT.connect(this.deployer).mint(this.users[1].address, 1)
 
-  it('mint function fails when minting with zero amount', async () => {
-    await expect(this.gen2.connect(this.deployer).mint(
-      0
-    )).to.revertedWith('gen2 : mint amount invalid')
-  })
-
-  it('mint function fails when minting with exceed amount', async () => {
-    await expect(this.gen2.connect(this.deployer).mint(
-      3500
-    )).to.revertedWith('gen2 : max limit')
-  })
-
-  it('mint function fails when minting legendary if not owner', async () => {
-    await expect(this.gen2.connect(this.users[0]).mint(
-      10
-    )).to.revertedWith('gen2: caller is not the owner')
-  })
-
-  it('mint function fails when minting projectgodjira.eth if not owner', async () => {
-    await expect(this.gen2.connect(this.users[0]).mint(
-      100
-    )).to.revertedWith('gen2: caller is not the owner')
-  })
-
-  it('mint function fails when minting public with exceed amount', async () => {
-    await expect(this.gen2.connect(this.users[0]).mint(
-      1000
-    )).to.revertedWith('gen2: public invalid amount')
-  })
-
-  it('mint function fails when minting public if creator try again', async () => {
-    await this.gen2.connect(this.deployer).mint(10)
-    await this.gen2.connect(this.deployer).mint(100)
-    await this.gen2.connect(this.users[1]).mint(1)
-
-    await expect(this.gen2.connect(this.users[1]).mint(
-      1
-    )).to.revertedWith('gen2: public invalid address')
+    await this.mockNFT.connect(this.users[1]).setApprovalForAll(this.genesis.address, true)
+    await this.genesis.connect(this.users[1]).claim([1])
   })
 
   it('mint function succeeds', async () => {
+
+    // Genesis holder
+    await this.gen2.connect(this.users[1]).purchase()
+    // const owner = await this.gen2.balanceOf(this.users[1].address)
+    const totalSupply = await this.gen2.totalSupply()
+    console.log(totalSupply)
+    // expect(tokenId.toString()).to.equal(new BN("10").toString())
+
+    return
+    
+    // Whitelist wallet
+    await this.gen2.connect(this.deployer).addWhiteList([this.users[2].address])
     await this.gen2.connect(this.users[2]).mint(1)
 
-    const tokenId = await this.gen2.nfts('0xF6f94e2faAb5D415f40a4755860C788df050a37c')
-    expect(tokenId.toString()).to.equal(new BN("10").toString())
+    
+
+    // Private sale buyer
+    await this.gen2.connect(this.users[3]).mint(1)
+    return;
+
+    // Core start
+    for(var i = 0; i < 31; i ++) {
+      await this.gen2.connect(this.deployer).mint(10)
+    }
+    // Core end
+
+    // Founder Shan start
+    await this.gen2.connect(this.deployer).mint(15)
+    // Founder Shan end
+
+    // Founder Jamie start
+    await this.gen2.connect(this.deployer).mint(15)
+    // Founder Jamie end
+
+
+    // const tokenId = await this.gen2.nfts('0xF6f94e2faAb5D415f40a4755860C788df050a37c')
+    // expect(tokenId.toString()).to.equal(new BN("10").toString())
   })
 
   it('setBaseURI function fails', async () => {
